7f541f7272c32e31166e7f99793294ba6ed4c7b0
heedahan16 <devhee.handa@gmail.com>
2023-09-24 16:09:44 +0900

    테슬라 키워드로 10페이지까지 수집하는 코드에 기사 제목, 날짜, 매체명, 본문, URL을 수집하는 함수를 연결하고 데이터프레임으로 만들기(마지막 페이지 기사 데이터만 데이터프레임으로 출력되는 에러: 해결)

--- News_Crawling5.py
+++ News_Crawling5.py
@@ -1,11 +1,15 @@
 # 테슬라 키워드로 10페이지까지 수집하는 코드 작성
 
+
 import requests
 from bs4 import BeautifulSoup
 import re
 import pandas as pd
 
 def get_news(keyword):
+
+    urls = []
+    Data = []
 
     for page in range(1, 11):
 
@@ -16,12 +20,10 @@ def get_news(keyword):
         res = requests.get(f"https://search.naver.com/search.naver?where=news&sm=tab_pge&query={keyword}&sort=2&photo=0&field=0&pd=1&ds=2023.09.17&de=2023.09.24&mynews=0&office_type=0&office_section_code=0&news_office_checked=&office_category=0&service_area=0&nso=so:r,p:1w,a:all&start={start}")
         soup = BeautifulSoup(res.text, "html.parser")
 
-        urls = []
         for a in soup.select("ul.list_news div.news_area a"):
             if a.text == "네이버뉴스":
                 urls.append(a["href"])
 
-        Data = []
         for url in urls:
             res = requests.get(url)
             soup = BeautifulSoup(res.text, "html.parser")
