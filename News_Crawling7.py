7e35cc070637e1849189e7e8c96a373cdf0972ec
heedahan16 <devhee.handa@gmail.com>
2023-09-25 17:22:01 +0900

    기간을 입력했을 때 하루씩 이동하면서 데이터를 수집하는 코드 작성(기간 for문 작성 시 에러: 발생)

--- News_Crawling7.py
+++ News_Crawling7.py
@@ -1,51 +1,56 @@
-# 기간을 입력했을 때, 하루씩 이동하면서 데이터를 수집하고 결과를 csv 파일로 저장하는 코드 작성
-
 import requests
 from bs4 import BeautifulSoup
 import re
 import pandas as pd
 
-def get_news(keyword, start_date, end_date):
+def get_news(url):
+    headers = {"Cookie":"NNB=OURWEDJJKXKGI; nx_ssl=2; _naver_usersession_=+VdPHYAMq5W53YOSu5ttbQ==; page_uid=ieZzndprvmsssdTDFjRssssssah-332673"}
 
-    urls = []
-    Data = []
+    res = requests.get(url, headers=headers)
+    soup = BeautifulSoup(res.text, "html.parser")
+    title = soup.select_one("h2#title_area").text
+    date = soup.select_one("div.media_end_head_info_datestamp span")["data-date-time"].split(" ")[0]
+    media = soup.select_one("a.media_end_head_top_logo img")["title"]
+    content = re.sub("\<br\/\>|[a-zA-Z]*\@[a-zA-Z]*\.co.kr|\<[^\>]*\>", "", str(soup.select_one("div#newsct_article"))).replace("    ", " ").replace("\n", "").strip()
+
+    return (title, date, media, content, url)
+
+
+def get_news_list(keyword, start_date, end_date):
+
+    news = []
 
     for date in pd.date_range(start_date, end_date):
-        print("date :", str(date).split(" ")[0])
+        str_date = date.strftime("%Y.%m.%d")
+        print("date: ", str_date)
 
         page = 1
         while True:
 
-            print("page: ", page)
+            startdate = start_date.replace(".", "")
+            enddate = end_date.replace(".", "")
 
-            start = (int(page) - 1) * 10 + 1
+            start = (page - 1) * 10 + 1
 
-            res = requests.get(f"https://search.naver.com/search.naver?where=news&sm=tab_pge&query={keyword}&sort=1&photo=0&field=0&pd=3&ds={start_date}&de={end_date}&mynews=0&office_type=0&office_section_code=0&news_office_checked=&office_category=0&service_area=0&nso=so:dd,p:from{start_date}to{end_date},a:all&start={start}")
+            headers = {"Cookie":"NNB=OURWEDJJKXKGI; nx_ssl=2; _naver_usersession_=+VdPHYAMq5W53YOSu5ttbQ==; page_uid=ieZzndprvmsssdTDFjRssssssah-332673"}
+            URL = f"https://search.naver.com/search.naver?where=news&sm=tab_pge&query={keyword}&sort=2&photo=0&field=0&pd=3&ds={start_date}&de={end_date}&mynews=0&office_type=0&office_section_code=0&news_office_checked=&office_category=0&service_area=0&nso=so:r,p:from{startdate}to{enddate},a:all&start={start}"
+            res = requests.get(URL, headers=headers)
             soup = BeautifulSoup(res.text, "html.parser")
 
-            if soup.select_one("div.not_found02") != None:
+            if soup.select_one("div.api_noresult_wrap") != None:
                 break
+
+            print("page: ", page)
+
+            news_list = soup.select("ul.list_news li")
+
+            for list in news_list:
+                if len(list.select("div.info_group a")) == 2:
+                    news.append(get_news(list.select("div.info_group a")[1]["href"]))
 
             page += 1
 
-            for a in soup.select("ul.list_news div.news_area a"):
-                if a.text == "네이버뉴스":
-                    urls.append(a["href"])
+    return pd.DataFrame(news, columns=["제목", "날짜", "매체명", "본문", "URL"])
 
-            for url in urls:
-                res = requests.get(url)
-                soup = BeautifulSoup(res.text, "html.parser")
-                
-                title = soup.select_one("h2#title_area > span").text
-                date = soup.select_one("div.media_end_head_info_datestamp span")["data-date-time"]
-                media = soup.select_one("div.media_end_head_top img.media_end_head_top_logo_img.light_type")["title"]
-                content = re.sub("\<br\/>|\<em.*\>.*\<\/em\>|[a-zA-Z]*\@[a-zA-Z]*\.com|뉴욕=.*|\<[^\>]*\>", "", str(soup.select_one("article#dic_area")).replace("\t", "").replace("\n", "").strip())
+print(get_news_list("테슬라", "2023.09.24", "2023.09.25"))
 
-                data = (title, date, media, content, url)
-                Data.append(data)
-
-    News = pd.DataFrame(Data, columns=["제목", "날짜", "매체명", "본문", "URL"])
-
-    return News.to_csv("News.csv", mode="w")
-
-print(get_news("테슬라", "2023.09.23", "2023.09.24"))
