eea1ed7c1332fec1f065e1b1ea01239c78460272
heedahan16 <devhee.handa@gmail.com>
2023-10-06 23:33:53 +0900

    정상 데이터, 스팸 데이터 개수 카운트

--- Spam_Filtering.py
+++ Spam_Filtering.py
@@ -1,19 +1,24 @@
 # 스팸 필터링을 NBC(나이브베이즈분류모델)을 쌩으로 구현해서 함수화하는 코드 작성
 
-training_set = [["me free lottery", 1], ["free get free you", 1],  ["you free sholarship", 0], ["free to contact me", 0], ["you won award", 0], ["you ticket lottery", 1]]
+training_set = [["me free lottery", 1], ["free get free you", 1],  ["you free scholarship", 0], ["free to contact me", 0], ["you won award", 0], ["you ticket lottery", 1]]
 test = ["free lottery"]
 
-Classfication = ["award", "contact", "free", "get", "lottery", "me", "scholarship", "ticket", "to", "won", "you"]
+Normal_Set = {}
+Spam_Set = {}
 
-Normal_Text = []
-Spam_Text = []
-    
-for training in training_set:
-    for text in training[0].split(" "):
-        if training[1] == 0:
-            Normal_Text.append(text)
-        else:
-            Spam_Text.append(text)
+for sentence, label in training_set:
+    if label == 0:
+        for text in sentence.split(" "):
+            if not text in Normal_Set:
+                Normal_Set[text] = 0
+            Normal_Set[text] += 1
 
-print(Normal_Text)
-print(Spam_Text)
+    else:
+        for text in sentence.split(" "):
+            if not text in Normal_Set:
+                Spam_Set[text] = 0
+            Spam_Set[text] += 1
+
+print("normal: ", Normal_Set)
+print("spam: ", Spam_Set)
+
